a2b5351f448267ae2cfda065ad2c7d4c56a0c9e0
il-shn <iliashnurenko@gmail.com>
2024-05-15 16:39:44 +0300

    registration_validation

--- js/registration.js
+++ js/registration.js
@@ -1,9 +1,11 @@
 window.onload = function () {
+    let checkName = (name)=>/[a-zA-Z]/g.test(name);
+    let checkNumber = (number)=>/^\+?[0-9]{10,12}$/.test(number);
+    let checkEmail = (email)=>/^([a-z\d\._-]+)@([a-z\d_-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i
+    .test(email);
+    let checkPassword = (password)=>/^(?=.*[a-z])(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*]).{10,30}$/.test(password);
+    
     let btn = document.querySelector('#btn')
-    let checkName = /[a-zA-Z]/g;
-    let checkNumber = /\+*[0-9]{10,12}/g;
-    let checkEmail = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})*(\.[a-z]{2,8})?$/;
-    let checkPassword = /^(?=.*[a-z])(?=.*\d)(?=.*[A-Z])(?=.*[!@#$%^&*]).{10,30}$/g;
     let inputFirstName = document.querySelector('#inputFirstName')
     let inputSecondName = document.querySelector('#inputSecondName')
     let inputDateOfBirth = document.querySelector('#inputDateOfBirth')
@@ -49,30 +51,25 @@ window.onload = function () {
     btn.addEventListener('click', function() {
         let errorMessage = '';   
 
-        if (!checkName.test(inputFirstName.value)) {
+        if (!checkName(inputFirstName.value)) {
             errorMessage += 'Invalid first name\n';
         }
-        console.log('inputFirstName', checkName, inputFirstName.value, checkName.test(inputFirstName.value));        
         
-        if (!checkName.test(inputSecondName.value)) {
+        if (!checkName(inputSecondName.value)) {
             errorMessage += 'Invalid second name\n';
         }
-        console.log('inputSecondName', checkName, inputSecondName.value, checkName.test(inputSecondName.value));
 
-        if (!checkEmail.test(inputEmail.value)) {
+        if (!checkEmail(inputEmail.value)) {
             errorMessage += 'Invalid email\n';
         }
-        console.log('inputEmail', checkEmail, inputEmail.value, checkEmail.test(inputEmail.value));
-
-        if (!checkNumber.test(inputPhone.value)) {
+        
+        if (!checkNumber(inputPhone.value)) {
             errorMessage += 'Invalid phone number\n';
         }
-        console.log('inputPhone', checkNumber, inputPhone.value,checkNumber.test(inputPhone.value));
 
-        if (!checkPassword.test(inputPassword1.value)) {
+        if (!checkPassword(inputPassword1.value)) {
             errorMessage += 'Invalid password\n';
         }
-        console.log('inputPassword1', checkPassword, inputPassword1.value, checkPassword.test(inputPassword1.value));
 
         if (inputPassword1.value !== inputPassword2.value) {
             errorMessage += 'Different passwords\n';
